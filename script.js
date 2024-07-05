1224462d5df6b953bbd19e13f9ce1feafe1fc235
AlissonClaro <allnclaro@outlook.com>
2024-07-05 16:22:52 -0300

    edit finish

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const result = document.querySelector('.result');
+const humanScore = document.querySelector('#human-score');
+const machineScore = document.querySelector('#machine-score');
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
 
 const playGame = (humanChoice) => {
-  gameResult(humanChoice, playMachine());
+  gameResult(humanChoice, playMachine())
 };
 
 const playMachine = () => {
   const choices = ['rock', 'paper', 'scissors'];
-  const choiceResult = Math.floor(Math.random() * 3);
-  return choices[choiceResult];
-};
+  const choiceResult = Math.floor(Math.random() * 3)
+  return choices[choiceResult]
+}
 
 const gameResult = (human, machine) => {
-  console.log('Humano: ' + human + ' Máquina: ' + machine);
+//   console.log('Humano: ' + human + ' Máquina: ' + machine)
 
   if (human === machine) {
-    result.innerHTML = 'Empate';
+    result.innerHTML = 'Empate'
   } else if (
     (human === 'rock' && machine === 'scissors') ||
     (human === 'paper' && machine === 'rock') ||
     (human === 'scissors' && machine === 'paper')
   ) {
-    result.innerHTML = 'Você Venceu';
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber;
+    result.innerHTML = 'Você Venceu'
   } else {
-    result.innerHTML = 'Máquina Venceu';
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber;
+    result.innerHTML = 'Máquina Venceu'
   }
-};
+}
