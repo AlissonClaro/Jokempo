c5eaf2565abcd7991d80a129f646a48a04d99f02
Alisson Claro <alissonclaroo@outlook.com>
2024-07-16 22:02:29 -0300

    EDIT ENUM

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ const machineScore = document.querySelector('#machine-score');
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+// humanScoreNumber -> Camel Case
+// human_score_number -> Snake Case
+
+
+const GAME_OPTIONS = {
+  ROCK: 'rock',
+  PAPER: 'paper',
+  SCISSORS: 'scissors'
+}
+
 const playGame = (humanChoice) => {
   gameResult(humanChoice, playMachine())
 };
 
 const playMachine = () => {
-  const choices = ['rock', 'paper', 'scissors'];
+  const choices = ['GAME_OPTIONS.ROCK', 'GAME_OPTIONS.PAPER', 'GAME_OPTIONS.SCISSORS'];
   const choiceResult = Math.floor(Math.random() * 3)
   return choices[choiceResult]
 }
@@ -21,9 +31,9 @@ const gameResult = (human, machine) => {
   if (human === machine) {
     result.innerHTML = 'Empate'
   } else if (
-    (human === 'rock' && machine === 'scissors') ||
-    (human === 'paper' && machine === 'rock') ||
-    (human === 'scissors' && machine === 'paper')
+    (human === 'GAME_OPTIONS.ROCK' && machine === 'GAME_OPTIONS.SCISSORS') ||
+    (human === 'GAME_OPTIONS.PAPER' && machine === 'GAME_OPTIONS.ROCK') ||
+    (human === 'GAME_OPTIONS.SCISSORS' && machine === 'GAME_OPTIONS.PAPER')
   ) {
     humanScoreNumber++
     humanScore.innerHTML = humanScoreNumber;
